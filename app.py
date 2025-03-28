bd0b735269702684386816a3bd075c9548671c50
abhashah23 <abhashah2305@gmail.com>
2025-03-29 01:05:24 +0530

    Update app.py
    fix: use correct port for Render

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ api.add_resource(TransitTodayResource, "/transits")
 api.add_resource(TransitReportResource, "/transits/report")
 api.add_resource(TransitUpcomingResource, "/transits/upcoming")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
