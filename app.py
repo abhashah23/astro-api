2e71870c234311cb18d89ba20d0e3fbd6f251b81
abhashah23 <abhashah2305@gmail.com>
2025-04-01 22:20:53 +0530

    Update app.py
    im tired

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask_restful import Api
-from resources import TransitTodayResource, TransitReportResource, TransitUpcomingResource
+from resources import (
+    TransitTodayResource,
+    TransitReportResource,
+    TransitUpcomingResource,
+    NatalChartResource
+)
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,14 +15,23 @@ api = Api(app)
 def home():
     return "Astrology API is live!"
 
-# API endpoints
+# Register API endpoints
 api.add_resource(TransitTodayResource, "/transits")
 api.add_resource(TransitReportResource, "/transits/report")
 api.add_resource(TransitUpcomingResource, "/transits/upcoming")
+api.add_resource(NatalChartResource, "/natal/chart")
 
+<<<<<<< HEAD
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
+=======
+# Run server
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+>>>>>>> 34cdf2c (Added natal chart endpoint)
